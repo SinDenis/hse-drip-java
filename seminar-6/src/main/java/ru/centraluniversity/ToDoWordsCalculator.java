4051ed3b4bb1ccd8f1eb216bd3a4db760e8a38d1
Aleksandr Novoselov <ale.a.novoselov@tbank.ru>
2026-02-18 15:18:12 +0300

    Seminar work.

--- seminar-6/src/main/java/ru/centraluniversity/ToDoWordsCalculator.java
+++ seminar-6/src/main/java/ru/centraluniversity/ToDoWordsCalculator.java
@@ -1,11 +1,47 @@
 package ru.centraluniversity;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ToDoWordsCalculator implements WordsCalculator {
 
     @Override
     public String getResult(Path... paths) {
+        try (Stream<String> lines = Arrays.stream(paths)
+                .flatMap(path -> {
+                            try {
+                                return Files.lines(path, Charset.forName("cp1251"))
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+
+                        }
+                )
+        ) {
+
+            // Старый подход
+            String[] words = {"пьер", "Пьер"};
+            String[] parsedWords = new String[2];
+            for (int i = 0; i < words.length; i++) {
+                parsedWords[i] = words[i].toLowerCase();
+            }
+
+            // Новый подход
+            String[] words2 = {"пьер", "Пьер"};
+            Arrays.stream(words2)
+                    .map(word -> word.toLowerCase())
+                    .collect(Collectors.toSet())
+            ;
+
+            // TODO Продолжите обрабатывать поток строк в переменной lines
+        }
+
+
         // TODO посчитать количество слов в текстах, расположенных по указанным путям.
         //  Результатом должен быть текст, в котором каждая строка является
         //  парой "слово в нижнем регистре - количество вхождений указанного слова в тексте".
