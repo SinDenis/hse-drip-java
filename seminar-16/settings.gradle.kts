b5db5a04522dfee95432652e34da9bf66c87712c
Aleksandr Novoselov <ale.a.novoselov@tbank.ru>
2026-05-20 13:13:43 +0300

    Added seminar-16 gradle files.

--- seminar-16/settings.gradle.kts
+++ seminar-16/settings.gradle.kts
@@ -1,1 +1,21 @@
+/*pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}*/
+
+/*dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+    }
+}*/
+
+/*buildCache {
+    local {
+        isEnabled = true
+    }
+}*/
+
 rootProject.name = "seminar-16"
